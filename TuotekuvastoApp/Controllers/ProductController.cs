25420f87da6a59cb57175af85197dbfc6f821190
Niko Skantsi <niko.skantsi@edu.karelia.fi>
2024-10-29 17:27:21 +0200

    Ready to Push

---
+++ TuotekuvastoApp/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using TuotekuvastoApp.Models;
+
+namespace TuotekuvastoApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Product()
+        {
+            var products = GetProducts();
+            return View(products);
+        }
+
+        private object GetProducts()
+        {
+            var json = System.IO.File.ReadAllText("wwwroot/products.json");
+
+            // Muutos alkuperäiseen. En asentanut erillistä NuGettia vaan käytän .NET olemassa olevaa System.Text.Json nimiavaruudesta löytyvää serializeria.
+
+            var options = new JsonSerializerOptions() 
+            { 
+                PropertyNameCaseInsensitive = true            
+            };
+
+            return JsonSerializer.Deserialize<List<Product>>(json, options);
+        }
+    }
+}
